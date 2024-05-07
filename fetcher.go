84db7eb17f2cbcaeb492b8a0f191ebc09c037050
Nikolay Ditchev <n.ditchev22@acsbg.org>
2024-05-07 11:47:44 +0300

    url fetching

--- fetcher.go
+++ fetcher.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fetcher/calls_proposals_urls"
+	"fetcher/cp_urls_fetcher"
 	"fetcher/grant_doc_fetcher"
 	"io"
 	"os"
@@ -28,7 +28,7 @@ func FetcherFactory(fetcherType string, data map[string]string) (fetcher Fetcher
 		fetcher, err = grant_doc_fetcher.NewFetcher(query, data["downloadFolderPath"])
 		return
 	case "urlFetcher":
-		fetcher, err = calls_proposals_urls.NewFetcher(query, data["downloadFolderPath"])
+		fetcher, err = cp_urls_fetcher.NewFetcher(query, data["downloadFolderPath"])
 	default:
 		return
 	}
